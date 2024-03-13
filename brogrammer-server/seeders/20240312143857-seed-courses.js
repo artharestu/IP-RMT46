9df30cc5023b33556b7fc7540fb03764019c8286
artharestu <artharestuabadi@gmail.com>
2024-03-13 14:53:33 +0700

    FIX: Seeding

--- brogrammer-server/seeders/20240312143857-seed-courses.js
+++ brogrammer-server/seeders/20240312143857-seed-courses.js
@@ -3,8 +3,15 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
+    const courses = require('../data/courses.json')
+    courses.forEach(course => {
+      course.createdAt = new Date();
+      course.updatedAt = new Date();
+    })
+    await queryInterface.bulkInsert('Courses', courses, {})
   },
 
   async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Courses', null, {})
   }
 };
